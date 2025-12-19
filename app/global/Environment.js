116a18eabf2ed5e24dea9147b7743433ac91db03
dchiragj <d.chirag.j@gmail.com>
2025-12-19 18:21:24 +0530

    -- add sound in token

--- app/global/Environment.js
+++ app/global/Environment.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 export function getBaseUrl() {
   // return 'http://localhost:8008';
-  return 'http://192.168.2.50:8008';
-  // return 'https://api-queueapp.clustersofttech.com';
+  // return 'http://192.168.2.50:8008';
+  return 'https://api-queueapp.clustersofttech.com';
 }
